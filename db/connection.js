624e23ca682d49dee07e06da954379521b0b794d
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-30 11:15:45 -0600

    created .env file

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,10 @@ const mysql = require("mysql2");
 
 // Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    password: '275gtb4!',
-    database: 'election'
-  });
+  host: 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 module.exports = db;
